1c8735d60afa0729c1b1d730577538fd4e95c21a
Gabriel Luiz <gabriells760@gmail.com>
2020-11-17 22:10:16 -0300

    Refactoring

--- src/controllers/bling_controller.js
+++ src/controllers/bling_controller.js
@@ -1,108 +1,57 @@
 require("dotenv").config();
 const axios = require("axios");
 var parseString = require('xml2js').parseString;
+const querystring = require('querystring');
+const js2xmlparser = require("js2xmlparser");
 
-module.exports = async (request, response, next) => {
-    var addressSplit = request.dataPipe.orgAddress.split(',');
-    var name = request.dataPipe.orgName;
-    var typePeople = 'J';
-    var address = addressSplit[1];
-    var cpfCnpj = '';
-    var ie = '';
-    var number = addressSplit[5];
-    var comp = addressSplit[4];
-    var neigh = addressSplit[3];
-    var city = addressSplit[1].split('-')[0];
-    var cep = addressSplit[2];
-    var uf = addressSplit[1].split('-')[1];
-    var phone = request.dataPipe.phoneOrg;
-    var email = request.dataPipe.email;
-    var value = request.dataPipe.valueDeal;
-    var nameDeal = request.dataPipe.nameDeal;
-    var xmlString = `<?xml version="1.0" encoding="UTF-8"?>
-    < pedido >
- <cliente>
- <nome>${name}</nome>
- <tipoPessoa>${typePeople}</tipoPessoa>
- <endereco>${address}</endereco>
- <cpf_cnpj>${cpfCnpj}</cpf_cnpj>
- <ie>${ie}</ie>
- <numero>${number}</numero>
- <complemento>${comp}</complemento>
- <bairro>${neigh}</bairro>
- <cep>${cep}</cep>
- <cidade>${city}</cidade>
- <uf>${uf}</uf>
- <fone>${phone}</fone>
- <email>${email}</email>
- </cliente>
- <transporte>
- <transportadora>Transportadora XYZ</transportadora>
- <tipo_frete>R</tipo_frete>
- <servico_correios>SEDEX - CONTRATO</servico_correios>
- <dados_etiqueta>
- <nome>Endereço de entrega</nome>
- <endereco>Rua Visconde de São Gabriel</endereco>
- <numero>392</numero>
- <complemento>Sala 59</complemento>
- <municipio>Bento Gonçalves</municipio>
- <uf>RS</uf>
- <cep>95.700-000</cep>
- <bairro>Cidade Alta</bairro>
- </dados_etiqueta>
- <volumes>
- <volume>
- <servico>SEDEX - CONTRATO</servico>
- <codigoRastreamento></codigoRastreamento>
- </volume>
- <volume>
- <servico>PAC - CONTRATO</servico>
- <codigoRastreamento></codigoRastreamento>
- </volume>
- </volumes>
- </transporte>
- <itens>
- <item>
- <codigo>001</codigo>
- <descricao>${nameDeal}</descricao>
- <un>Pç</un>
- <qtde>1</qtde>
- <vlr_unit>${value}</vlr_unit>
- </item>
- </itens>
- <parcelas>
- <parcela>
- <data>01/09/2009</data>
- <vlr>100</vlr>
- <obs>Teste obs 1</obs>
- </parcela>
- <parcela>
- <data>06/09/2009</data>
- <vlr>50</vlr>
- <obs></obs>
- </parcela>
- <parcela>
- <data>11/09/2009</data>
- <vlr>50</vlr>
- <obs>Teste obs 3</obs>
- </parcela>
- </parcelas>
- <vlr_frete>15</vlr_frete>
- <vlr_desconto>10</vlr_desconto>
- <obs>Testando o campo observações do pedido</obs>
- <obs_internas>Testando o campo observações internas do pedido</obs_internas>
-</pedido > `;
-    var xml = '';
-    parseString(xmlString, function (err, result) {
-        xml = result;
-    });
-    var post = {
-        apikey: process.env.API_KEY_BLING,
-        xml: xml
-    };
+module.exports = (request, response, next) => {
+    for (var i = 0; i < request.body.response.data.length; i++) {
+
+        let idDeal = request.body.response.data[i].id;
+        let value = request.body.response.data[i].value;
+        let statusDeal = request.body.response.data[i].status;
+        let name = request.body.response.data[i].org_id.name;
+        let email = request.body.response.data[i].org_id.cc_email;
+        let nameDeal = request.body.response.data[i].title;
+
+        if (statusDeal == "won") {
+            const order = {
+                cliente: {
+                    nome: name,
+                    tipoPessoa: 'J',
+                    email: email,
+                },
+                itens: [
+                    {
+                        item: {
+                            codigo: idDeal,
+                            descricao: nameDeal,
+                            un: 'un',
+                            qtde: 1,
+                            vlr_unit: value,
+                        },
+                    },
+                ]
+            };
+
+            const orderXml = encodeURIComponent(js2xmlparser.parse('pedido', order, { declaration: { encoding: 'UTF-8' } }));
 
 
-    const resp = await axios.post('https://bling.com.br/Api/v2/pedido/json/', post)
-    return response.status(200).send({ data: resp });
+            var post = querystring.stringify({
+                apikey: process.env.API_KEY_BLING,
+                xml: orderXml
+            });
+
+            console.log(process.env.API_KEY_BLING);
+            const resp = axios.post('https://bling.com.br/Api/v2/pedido/json/', post);
+        }
+        else {
+            console.log('Oportunidade não está ganha!');
+        }
+
+    }
+
+
+    return response.status(200).send({ data: "resp" });
 
 };
--- src/middleware/middleware_integration.js
+++ src/middleware/middleware_integration.js
@@ -4,41 +4,18 @@ require("dotenv").config();
 
 module.exports = async (request, response, next) => {
     try {
-        const { data } = await axios.get('https://teste69.pipedrive.com/api/v1/deals?limit=500&api_token=' + process.env.API_TOKEN)
-        var lenght = data.data.lenght;
-        for (var i = 0; i < lenght; i++) {
-            var idDeal = data.data[i].id;
-            var valueDeal = data.data[i].value;
-            var addDateDeal = data.data[i].add_time;
-            var closeDateDeal = data.data[i].close_time;
-            var statusDeal = data.data[i].status;
-            var orgName = data.data[i].org_id.name;
-            var orgAddress = data.data[i].org_id.address;
-            var orgEmail = data.data[i].org_id.cc_email;
-            var nameDeal = data.data[i].title;
-            var ownerName = data.data[i].owner_name;
-            var phoneOrg = data.data[i].person_id.phone.value;
+        const { data: response } = await axios.get('https://teste69.pipedrive.com/api/v1/deals?limit=500&api_token=' + process.env.API_TOKEN)
+
+        for await (const data of response.data) {
+
             const dataPipe = {
-                idDeal,
-                valueDeal,
-                addDateDeal,
-                closeDateDeal,
-                statusDeal,
-                orgName,
-                orgAddress,
-                orgEmail,
-                nameDeal,
-                ownerName,
-                phoneOrg,
+                response
             };
-            if (statusDeal == "won") {
-                request = { ...request, dataPipe };
-                return next();
-            }
-            else {
-                console.log('Oportunidade não está ganha!');
-            }
+            request.body = dataPipe;
+            return next();
+
         }
+
 
     } catch (error) {
         console.log(error);
